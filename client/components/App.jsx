b378e07e8c4de2f1150f0424e81450480799b57f
Yingbo (Max) Wang <maxwyb@gmail.com>
2017-08-07 16:45:37 -0700

    Initial implementation of user login, by following Cognito SDK's doc.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-    
+import { CognitoUserPool, CognitoUserAttribute, CognitoUser } from 'amazon-cognito-identity-js';
+
 export class App extends React.Component {
   render() {
     return (
@@ -9,11 +10,16 @@ export class App extends React.Component {
   }
 }
 
-var AWS = require('aws-sdk');
+let COGNITO_REGION = 'us-east-1';
+let COGNITO_IDENTITY_POOL_ID = 'us-east-1:ca5605a3-8ba9-4e60-a0ca-eae561e7c74e';
+let COGNITO_USER_POOL_ID = 'us-east-1_yyImSiaeD';
+let COGNITO_CLIENT_ID = '4dc7abqcmfsbi6v00765cohu8p';  // Aquaint-web client
+
 // Initialize the Amazon Cognito credentials provider
-AWS.config.region = 'us-east-1'; // Region
+var AWS = require('aws-sdk');
+AWS.config.region = COGNITO_REGION; // Region
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: 'us-east-1:ca5605a3-8ba9-4e60-a0ca-eae561e7c74e',
+    IdentityPoolId: COGNITO_IDENTITY_POOL_ID,
 });
 
 export class UserLoginForm extends React.Component {
@@ -34,6 +40,8 @@ export class UserLoginForm extends React.Component {
 
     handleChange(event) {
 	// Update React state when the form's input changes
+	// For example, when user types 'a' into the password field
+	// event.target.name = password, and event.target.value = a
 	this.setState({[event.target.name]: event.target.value});
     }
     
@@ -42,6 +50,47 @@ export class UserLoginForm extends React.Component {
 	// Prevent broswer from sending a HTTP GET request with parameters
 	// in form (and perhaps refreshing the page)
 	event.preventDefault();
+
+	// Connect to AWS Cognito to authenticate user
+	var authenticationData = {
+            Username : this.state.username,
+            Password : this.state.password,
+	};
+	var authenticationDetails = new AWSCognito.CognitoIdentityServiceProvider.AuthenticationDetails(authenticationData);
+	var poolData = {
+            UserPoolId : COGNITO_USER_POOL_ID,
+            ClientId : COGNITO_CLIENT_ID
+	};
+	var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
+	var userData = {
+            Username : this.state.username,
+            Pool : userPool
+	};
+	var cognitoUser = new AWSCognito.CognitoIdentityServiceProvider.CognitoUser(userData);
+	cognitoUser.authenticateUser(authenticationDetails, {
+            onSuccess: function (result) {
+		console.log('access token + ' + result.getAccessToken().getJwtToken());
+
+		let awsLoginKey =
+		    'cognito-idp.' + COGNITO_REGION + '.amazonaws.com/'
+		    + COGNITO_USER_POOL_ID;
+		AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+                    IdentityPoolId : COGNITO_IDENTITY_POOL_ID,
+                    Logins : {
+			// Change the key below according to the specific region your user pool is in.
+			awsLoginKey : result.getIdToken().getJwtToken()
+                    }
+		});
+
+		// Instantiate aws sdk service objects now that the credentials have been updated.
+		// example: var s3 = new AWS.S3();
+
+            },
+
+            onFailure: function(err) {
+		alert(err);
+            },
+	});
     }
     
     render() {
