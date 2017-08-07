d6cc16013c836e303ce23c55e50120ace28a1ec4
Yingbo (Max) Wang <maxwyb@gmail.com>
2017-08-07 16:07:45 -0700

    Install aws-sdk and amazon-cognito-identity-js. Create a HTML form in React by Controlled Components.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,59 @@
 import React from 'react';
-
-export default class App extends React.Component {
+    
+export class App extends React.Component {
   render() {
     return (
       <div style={{textAlign: 'center'}}>
         <h1>Hello World</h1>
       </div>);
   }
+}
+
+var AWS = require('aws-sdk');
+// Initialize the Amazon Cognito credentials provider
+AWS.config.region = 'us-east-1'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'us-east-1:ca5605a3-8ba9-4e60-a0ca-eae561e7c74e',
+});
+
+export class UserLoginForm extends React.Component {
+    // Make the form to be "Controlled Components"
+    // that is, making the React state be the "single source of truth"
+    // See: https://facebook.github.io/react/docs/forms.html
+    constructor(props){
+	super(props);
+	this.state = {
+	    username: '',
+	    password: ''
+	};
+	
+	// Resolve error of "Cannot read property 'setState' of undefined"
+	this.handleChange = this.handleChange.bind(this);
+	this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+	// Update React state when the form's input changes
+	this.setState({[event.target.name]: event.target.value});
+    }
+    
+    handleSubmit(event) {
+	console.log('User tried to log in: ' + this.state.username + '; ' + this.state.password);
+	// Prevent broswer from sending a HTTP GET request with parameters
+	// in form (and perhaps refreshing the page)
+	event.preventDefault();
+    }
+    
+    render() {
+	return (
+	    <form onSubmit={this.handleSubmit}>
+	      Username:
+	      <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
+	      Password:
+	      <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
+	      Submit:
+	      <input type="submit" value="Log In"/>
+	    </form>
+	);
+    }
 }
--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import { App, UserLoginForm } from './components/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<UserLoginForm />, document.getElementById('userlogin'));
