80d3facc916b8f22e3dd65a12db0058261cf86b7
Wandi <lwd726@gmail.com>
2017-08-07 21:39:25 -0700

    styled landing page forms

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,99 @@
 import React from 'react';
+import styled from 'styled-components';
 import { CognitoUserPool, CognitoUserAttribute, CognitoUser, AuthenticationDetails } from 'amazon-cognito-identity-js';
+
+/*                                   UI ELEMENTS                              */
+
+// DIV
+const Div = styled.div`
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  text-align: center;
+  font-weight: 350;
+  font-size: 0.8rem;
+  color:#95989A;
+  margin-top: 3em;
+`
+
+// GREETING
+const H1 = styled.h1`
+  font-weight: 300;
+  font-size:2.5rem;
+  color: #2F3541;
+`
+
+// INPUT FORM
+const Input = styled.input`
+  padding: 0.75rem;
+  margin:0.5rem;
+  max-width: 30em;
+  height: 3em;
+  width: 21em;
+  border-style: solid;
+  border-width: 0.25px;
+  border-color: #95989A;
+  background-color:transparent;
+  border-radius: 5px;
+`
+
+// CONTINUE/LOGIN/SIGNUP BUTTONS
+const Button = styled.input`
+  letter-spacing: 0.5px;
+  font-weight:300;
+  color: white;
+  padding: 0.3rem;
+  margin:0.5rem;
+  max-width: 30em;
+  height: 3em;
+  width: 15em;
+  border-style: solid;
+  border-width: 0.25px;
+  border-color: #0E85AC;
+  background-color:#0E85AC;
+  border-radius: 50px;
+`
+// FACEBOOK LOGIN BUTTON
+const FButton = styled.input`
+  letter-spacing: 0.5px;
+  font-weight:300;
+  padding: 0.3rem;
+  color: #95989A;
+  margin:0.5rem;
+  max-width: 30em;
+  height: 3em;
+  width: 15em;
+  border-style: solid;
+  border-width: 0.25px;
+  border-color: #95989A;
+  background-color:transparent;
+  border-radius: 50px;
+`
+// FORM SECTION
+const Form = styled.form`
+  padding-top: 1.25em;
+`
+
+// TEXT INSTRUCTIONS
+const Instruction = styled.p`
+  padding-bottom: .75em;
+  font-size: 1.25em;
+  font-weight: 300;
+`
+
+const Subtitle = styled.p`
+  margin-top: -1em;
+  padding-bottom: .75em;
+  font-size: 1.5em;
+  font-weight: 300;
+`
+
+/*                            END OF UI ELEMENTS                              */
 
 export class App extends React.Component {
   render() {
     return (
-      <div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
-      </div>);
+      <Div style={{marginTop: '10%'}}>
+        <img src={'./app_demo_landing.png'} alt="clocks" ></img>
+      </Div>);
   }
 }
 
@@ -32,7 +119,7 @@ export class UserLoginForm extends React.Component {
 	    username: '',
 	    password: ''
 	};
-	
+
 	// Resolve error of "Cannot read property 'setState' of undefined"
 	this.handleChange = this.handleChange.bind(this);
 	this.handleSubmit = this.handleSubmit.bind(this);
@@ -44,7 +131,7 @@ export class UserLoginForm extends React.Component {
 	// event.target.name = password, and event.target.value = a
 	this.setState({[event.target.name]: event.target.value});
     }
-    
+
     handleSubmit(event) {
 	console.log('User tried to log in: ' + this.state.username + '; ' + this.state.password);
 	// Prevent broswer from sending a HTTP GET request with parameters
@@ -93,17 +180,41 @@ export class UserLoginForm extends React.Component {
             },
 	});
     }
-    
+
     render() {
 	return (
-	    <form onSubmit={this.handleSubmit}>
-	      Username:
-	      <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
-	      Password:
-	      <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
-	      Submit:
-	      <input type="submit" value="Log In"/>
-	    </form>
+    // <Div className={'w-50'}>
+    //   <H1>Welcome back!</H1>
+    //   <form onSubmit={this.handleSubmit}>
+    //    <Input type="text" name="username" placeholder="Username" value={this.state.username} onChange={this.handleChange}/>
+    //    <br/>
+    //    <Input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange}/>
+    //   <br/><br/>
+    //    <Button type="submit" value="Log In"/>
+    //  </form>
+    // </Div>
+
+    // SignUP Form Mockup *REMEMBER TO CHANGE ELEMENT TYPES & NAMES & SIGNIN/LOGIN Methods
+
+    <Div className={'w-50'}>
+      <H1>Let's get Aquainted.</H1>
+      <Subtitle>All of your social media profiles in one place.</Subtitle>
+      <Form onSubmit={this.handleSubmit}>
+        <Instruction> Sign up with... </Instruction>
+        <FButton type="submit" value="Facebook" />
+      </Form>
+      <Form onSubmit={this.handleSubmit}>
+        <Instruction> Or, sign up with email </Instruction>
+       <Input type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange}/>
+       <br/>
+       <Input type="text" name="realname" placeholder="Full Name" value={this.state.realname} onChange={this.handleChange}/>
+      <br/>
+       <Button type="submit" value="Continue"/>
+     </Form>
+     <Form onSubmit={this.handleSubmit}>
+       <Instruction>Already registered? <a href="signin">Sign in here</a></Instruction>
+     </Form>
+    </Div>
 	);
     }
 }
