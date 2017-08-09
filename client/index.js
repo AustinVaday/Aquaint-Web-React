b362637ee63dbcc80f69e379a7298777a8be7d75
Yingbo (Max) Wang <maxwyb@gmail.com>
2017-08-09 11:26:50 -0700

    Modify webpack-dev-server content base so it serves CSS and JS correctly. Entire website mostly functional, except CSS for React components.

--- client/index.js
+++ client/index.js
@@ -2,5 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App, UserLoginForm, UserSignupForm } from './components/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<UserSignupForm />, document.getElementById('userlogin'));
