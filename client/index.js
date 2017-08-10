5fa41a1e9738974f2e6dd9390fa7521f118440cb
Yingbo (Max) Wang <maxwyb@gmail.com>
2017-08-09 17:22:49 -0700

    Add react-router modules. Delete comments on index.html and beautify codes and indentations.

--- client/index.js
+++ client/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { UserSignupForm } from './components/UserSignupForm.jsx';
+
+class Example extends React.Compoenent {
+    render() {
+	return (
+	    <div>
+	      Hello React!
+	    </div>
+	);
+    }
+}
 
 ReactDOM.render(<UserSignupForm />, document.getElementById('userAuth'));
 
