d9b8161fb96c617caa21f64583ba43bee10ba31f
Yingbo (Max) Wang <maxwyb@gmail.com>
2017-08-09 18:26:22 -0700

    Initial attempt at client-side routing by react-router. Seems that it's best to wrap our landing page in a big JSX element to handle it and user profiles together.

--- client/index.js
+++ client/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { UserSignupForm } from './components/UserSignupForm.jsx';
 
-class Example extends React.Compoenent {
-    render() {
+class Example extends React.Component {
+    render(match) {
 	return (
 	    <div>
-	      Hello React!
+	      Hello, ${this.props.location.pathname}!
 	    </div>
 	);
     }
 }
 
-ReactDOM.render(<UserSignupForm />, document.getElementById('userAuth'));
+ReactDOM.render((
+    <Router>
+      <div>
+	<Route exact path='/' component={UserSignupForm}/>
+	<Route path='/' component={Example}/>
+      </div>
+    </Router>
+), document.getElementById('userAuth'));
 
